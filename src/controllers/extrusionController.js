4837ca8329aaaede63e67047d7e824275f0dee73
miguel bastidas <m.alejandro_80@hotmail.com>
2020-01-08 23:11:42 -0400

    acomodando middleware convertUnids

--- src/controllers/extrusionController.js
+++ src/controllers/extrusionController.js
@@ -10,109 +10,113 @@ controller.getArea = (req, res) => {
   return res.json(data);
 };
 
-controller.calcRe = (Area_i, Area_f) => {
-  return Area_i*1 / Area_f*1;
+controller.calcRe = (diametro_i, diametro_f) => {
+  return controller.calcArea(diametro_i) / controller.calcArea(diametro_f) ;
 }
 
-controller.calcDefIdeal = (area_i, area_f) => {
-  return Math.log(area_i/area_f);
+controller.calcDefIdeal = (diametro_i, diametro_f) => {
+  re = controller.calcRe(diametro_i, diametro_f)
+  return Math.log(re);
 }
 
 controller.getRelacionExtruccion = (req,res)=>{
-  let data = { 're': controller.calcRe(req.body.area_i, req.body.area_f)}
+  let data = { 're': controller.calcRe(req.body.diametro_i, req.body.diametro_f)}
   return res.json(data);
 };
 
 controller.getJhonson = (req, res) => {
-  let data = { 'jhonson': controller.calcJohnson(req.body.area_i, req.body.area_f, req.body.a, req.body.b) }
+  let data = { 'jhonson': controller.calcJohnson(req.body.diametro_i, req.body.diametro_f, req.body.a, req.body.b) }
   return res.json(data);
 };
 
 
-controller.calcJohnson = (area_i, area_f, a,b)=>{
-  let Re = controller.calcRe(area_i,area_f);
+controller.calcJohnson = (diametro_i, diametro_f, a,b)=>{
+  let Re = controller.calcRe(diametro_i,diametro_f);
 
   return (a*1) + ((b*1)*(Math.log(Re)));
 }
 
-controller.calcEsfuerzoMedio = (esf_fluencia, n, area_i, area_f) => {
+controller.calcEsfuerzoMedio = (esf_fluencia, n, diametro_i, diametro_f) => {
    n = n*1;
    esf_fluencia = esf_fluencia*1;
-  return (esf_fluencia * Math.pow(controller.calcDefIdeal(area_i, area_f),n))/(1 +n);
+  return (esf_fluencia * Math.pow(controller.calcDefIdeal(diametro_i, diametro_f),n))/(1 +n);
 }
 
 controller.getEsfuerzoMedio = (req,res)=>{
-  let data = { 'esfuerzo_medio': controller.calcEsfuerzoMedio(req.body.esf_fluencia, req.body.n,req.body.area_i, req.body.area_f) }
+  let data = { 'esfuerzo_medio': controller.calcEsfuerzoMedio(req.body.esf_fluencia, req.body.n,req.body.diametro_i, req.body.diametro_f) }
   return res.json(data);
 };
 
-controller.calcExtrusionInd = (esf_fluencia, n, area_i, area_f, a, b) => {
+controller.calcExtrusionInd = (esf_fluencia, n, diametro_i, diametro_f, a, b) => {
   
-  let esfuerzo_medio = controller.calcEsfuerzoMedio(esf_fluencia, n, area_i, area_f);
-  let jhonson = controller.calcJohnson(area_i, area_f, a, b);
+  let esfuerzo_medio = controller.calcEsfuerzoMedio(esf_fluencia, n, diametro_i, diametro_f);
+  let jhonson = controller.calcJohnson(diametro_i, diametro_f, a, b);
 
   return jhonson * esfuerzo_medio;
 }
 
 controller.getExtruccionInd = (req,res) => {
-  let data = { 'presion_extrusion_indirecta': controller.calcExtrusionInd(req.body.esf_fluencia, req.body.n, req.body.area_i, req.body.area_f, req.body.a, req.body.b) }
+  let data = { 'presion_extrusion_indirecta': controller.calcExtrusionInd(req.body.esf_fluencia, req.body.n, req.body.diametro_i, req.body.diametro_f, req.body.a, req.body.b) }
   return res.json(data);
 }
 
-controller.calcFuerzaInd = (esf_fluencia, n, area_i, area_f, a, b) =>{
-  let pExtrusionInd = controller.calcExtrusionInd(esf_fluencia, n, area_i, area_f, a, b);
+controller.calcFuerzaInd = (esf_fluencia, n, diametro_i, diametro_f, a, b) =>{
+  let pExtrusionInd = controller.calcExtrusionInd(esf_fluencia, n, diametro_i, diametro_f, a, b);
+
+  area_i = controller.calcArea(diametro_i);
+  area_f = controller.calcArea(diametro_f);
 
   return pExtrusionInd * (area_i - area_f);
 
 }
 
 controller.GetFuerzaInd = (req,res) => {
-  let data = { 'Fuerza_indirecta': controller.calcFuerzaInd(req.body.esf_fluencia, req.body.n, req.body.area_i, req.body.area_f, req.body.a, req.body.b) }
+  let data = { 'Fuerza_indirecta': controller.calcFuerzaInd(req.body.esf_fluencia, req.body.n, req.body.diametro_i, req.body.diametro_f, req.body.a, req.body.b) }
   return res.json(data);
 }
 
-controller.calcPotenciaInd = (esf_fluencia, n, area_i, area_f, a, b,vel) => {
-  return controller.calcExtrusionInd(esf_fluencia, n, area_i, area_f, a, b) * vel;
+controller.calcPotenciaInd = (esf_fluencia, n, diametro_i, diametro_f, a, b,vel) => {
+  return controller.calcExtrusionInd(esf_fluencia, n, diametro_i, diametro_f, a, b) * vel;
 }
 
 controller.GetCalcInd = (req,res) => {
   let data = { 'Fuerza_indirecta': controller.calcFuerzaInd(
                                                             req.body.esf_fluencia, 
                                                             req.body.n, 
-                                                            req.body.area_i, 
-                                                            req.body.area_f, 
+                                                            req.body.diametro_i, 
+                                                            req.body.diametro_f, 
                                                             req.body.a, 
                                                             req.body.b
                                                           ), 
                 'presion_extrusion_indirecta': controller.calcExtrusionInd(
                                                             req.body.esf_fluencia, 
                                                             req.body.n, 
-                                                            req.body.area_i, 
-                                                            req.body.area_f, 
+                                                            req.body.diametro_i, 
+                                                            req.body.diametro_f, 
                                                             req.body.a, 
                                                             req.body.b
                                                             ),
                 'esfuerzo_medio': controller.calcEsfuerzoMedio(
                                                             req.body.esf_fluencia, 
                                                             req.body.n, 
-                                                            req.body.area_i, 
-                                                            req.body.area_f
+                                                            req.body.diametro_i, 
+                                                            req.body.diametro_f
                                                             ),
                 're': controller.calcRe(
-                                        req.body.area_i, 
-                                        req.body.area_f
+                                        req.body.diametro_i, 
+                                        req.body.diametro_f
                                         ),
                 'jhonson': controller.calcJohnson(
-                                                  req.body.area_i, 
-                                                  req.body.area_f, 
+                                                  req.body.diametro_i, 
+                                                  req.body.diametro_f, 
                                                   req.body.a, 
                                                   req.body.b
                                                 ),
                 'Potencia' : controller.calcPotenciaInd(
                                                         req.body.esf_fluencia,
                                                         req.body.n,
-                                                        req.body.area_i,
-                                                        req.body.area_f,
+                                                        req.body.diametro_i,
+                                                        req.body.diametro_f,
                                                         req.body.a,
                                                         req.body.b,
                                                         req.body.velocidad
--- src/middlewares/convertUnids.js
+++ src/middlewares/convertUnids.js
@@ -1,29 +1,76 @@
 const unidades_model = require("../models/unidades_model");
-
+const materiales = require("../models/propiedades_materiales_model");
 const middleware = {}
 
-middleware.Unids = (req,res,next) => {
-  data_convert = {};
-
-  middleware.convertUnid(data_convert, req.body.u_diametro_f, req.body.diametro_f, 'diametro_f', 'u_diametro_f');
+middleware.Unids = async (req,res,next) => {
+ let unidades = [];
   
-  // middleware.convertUnid(data_convert, req.body.u_diametro_i, req.body.diametro_i, 'diametro_i', 'u_diametro_i');
+  await middleware.convertUnid(req.body.u_diametro_i, req.body.diametro_i)
+    .then(data => {
+      unidades.push({
+        'diametro_i': data.conversion,
+        'u_diametro_i': data.unidad_conversion,
+      })
+    }); 
 
-  // middleware.convertUnid(data_convert, req.body.u_velocidad, req.body.velocidad, 'velocidad', 'u_velocidad');
+  await middleware.convertUnid(req.body.u_diametro_f, req.body.diametro_f)
+    .then(data => {
+      unidades.push({
+        'diametro_f': data.conversion,
+        'u_diametro_f': data.unidad_conversion,
+      })
+    });
+
+  await middleware.convertUnid(req.body.u_velocidad, req.body.velocidad)
+    .then(data => {
+      unidades.push({
+        'velocidad': data.conversion,
+        'u_velocidad': data.unidad_conversion,
+      })
+    });
+
+   material = await materiales.get(req.body.material);
+   
+   await middleware.convertUnid(material[0].unidad_esfuerzo_fluencia, material[0].esfuerzo_fluencia)
+    .then(data => {
+      unidades.push({
+        'esf_fluencia': data.conversion,
+        'u_esfuerzo_fluencia': data.unidad_conversion,
+        'n' : material[0].n
+
+      })
+    });
+
+    unidades = {
+      'diametro_i': unidades[0].diametro_i,
+      'u_diametro_i': unidades[0].u_diametro_i,
+      'diametro_f': unidades[1].diametro_f,
+      'u_diametro_f': unidades[1].u_diametro_f,
+      'velocidad': unidades[2].velocidad,
+      'u_velocidad': unidades[2].u_velocidad,
+      'esf_fluencia': unidades[3].esf_fluencia,
+      'u_esfuerzo_fluencia': unidades[3].u_esfuerzo_fluencia,
+      'n': unidades[3].n,
+      'a': req.body.a,
+      'b': req.body.b,
+    }
+
+    res.json(unidades);
+   next();
 
 }
 
-middleware.convertUnid = (object,u_convert,u_value,variable,u_variable) => {
-
-  unidades_model.getUnid(u_convert)
-    .then((data,object,variable,u_variable) => {
-      object = {
-        variable : data[0].equivalente_SI * (u_value * 1),
-        u_variable : data[0].unidad_equivalente_SI
-      }
-      console.log(object);
-    });
-
+middleware.convertUnid = (u_convert,u_value) => {
+  return new Promise((resolved,rejected)=>{ 
+    unidades_model.getUnid(u_convert)
+      .then((data) => {
+        object = {
+          conversion : data[0].equivalente_SI * (u_value * 1),
+          unidad_conversion : data[0].unidad_equivalente_SI
+        }
+        resolved(object);
+      })
+  });
 }
 
 module.exports = middleware;
