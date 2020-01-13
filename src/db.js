1b879402fe3a935730322266b99d92a98ad9c57a
miguel bastidas <m.alejandro_80@hotmail.com>
2020-01-12 20:09:42 -0400

    cambio en las credenciales database

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 // credenciales
 module.exports = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "tesis"
+  host: "bxc6o4spcg495zre20jc-mysql.services.clever-cloud.com",
+  user: "uaugi2guymjwvn3e",
+  password: "aNV37Qju4QPVFVFWdWtC",
+  database: "bxc6o4spcg495zre20jc"
 });
